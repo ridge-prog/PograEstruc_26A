e8fa19a8616cd3be3183197dd474e3d955825172
ridge-prog <rene.lopez5272@alumnos.udg.mx>
2026-02-13 17:27:02 -0500

    Update 6_NumeroPrimo.c

--- VIERNES13FEBRERO/6_NumeroPrimo.c
+++ VIERNES13FEBRERO/6_NumeroPrimo.c
@@ -12,3 +12,24 @@ int main()
         printf("Numero primo.");}
     return 0;
 }
+
+/*
+correcion de codigo
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, i;
+    printf("Ingresa un numero: \n");
+    scanf("%d", &num);
+
+    if (num<=1){
+        return 0;}
+    
+    if (num%2!=0||num%3!=0){
+        printf("Numero primo.");}
+    return 0;
+}
+*/
