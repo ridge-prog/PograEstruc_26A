0bebbf62cac100f5b926513837c8ea76cb67fd92
ridge-prog <rene.lopez5272@alumnos.udg.mx>
2026-02-13 17:11:38 -0500

    Create 4_NumPar.c

---
+++ VIERNES13FEBRERO/4_NumPar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("Ingresa un numero: \n");
+    scanf("%d", &num);
+
+    if (num%2==0){
+        printf("Numero par.");}
+    return 0;
+}
