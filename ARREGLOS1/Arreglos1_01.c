6a04bcaba20b65c4a8e8d2e222fc91ff2e4bc45f
ridge-prog <rene.lopez5272@alumnos.udg.mx>
2026-03-06 08:23:53 -0600

    Update Arreglos1_01.c

--- ARREGLOS1/Arreglos1_01.c
+++ ARREGLOS1/Arreglos1_01.c
@@ -15,11 +15,21 @@ int main()
     //Por cada brinco de datos, la direccion decrementa 4 bits
     //TIPO FLOAT (4 BITS), ETIQUETA TEMP DIR, CARACTER [24]; BITS DE TIPO X CARACTER = 96
     //se empieza desde 0?, temp[0]
-    //ACTIVIDAD 1
+    /* ACTIVIDAD 1
     printf("Direccion de memoria de 2 flotantes:\n&temp1:%i,\n&temp2:%i\n", &temp1, &temp2);
     printf("Direccion de temp:%i\n", temp);
     printf("Direccion de &temp[24]:%i\n", &temp[23]);
     printf("Direccion de &dummy:%i\n", &dummy);
+    */
+
+    /* ACTIVIDAD 2
+    printf("temp[1]:%f, &temp[1]%i\n", temp[1], &temp[1]);
+    printf("temp[2]:%f, &temp[2]%i\n", temp[2], &temp[2]);
+    */
+    
+    for(i=0; i<24; i++){
+        printf("Temperatura %i: %i\n", i, &temp[i]);
+    }
 
     return 0;
 }
