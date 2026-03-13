c34877de5693702385b22741c2022e4491ff45bb
ridge-prog <rene.lopez5272@alumnos.udg.mx>
2026-03-13 07:32:18 -0600

    Update and rename text to string.c

---
+++ 13_03_2026REAL/string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char alumno1[16]="Antonio";
+    char alumno2[16];
+    scanf("%s", &alumno2);
+    printf("%s\n", alumno1);
+    printf("%s\n", alumno2);
+    return 0;
+}
